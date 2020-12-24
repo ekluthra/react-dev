75cc8f71a88876b7084c89bd2230c4a5d87a215f
ekluthra <ekluthra@yahoo.com>
2020-12-23 22:16:09 -0500

    used useEffect to get recipes for chicken

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect, useState} from "react";
 
 const App = () => {
 
   const ID = process.env.REACT_APP_APP_ID;
   const KEY = process.env.REACT_APP_APP_KEY;
+  
+  //useeffect is just a function that is going to run every time the page loads
+  useEffect( () => {
+    getRecipes();
+  }, []); //the , [] will make it so it runs only once, when the page first loads
+  //adding the state in the square brackets makes it so that it runs when the value changes
+  
+  const getRecipes = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${ID}&app_key=${KEY}`);
+  
+    const data = await response.json();
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${ID}&app_key=${KEY}`;
-
+    console.log(data);
+  };
 
   return(
     <div className="App">
