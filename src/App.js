4b6ce2d90208194acee037b31ad8265bdc516368
ekluthra <ekluthra@yahoo.com>
2020-12-23 22:55:47 -0500

    implemented search feature

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ const App = () => {
   const KEY = process.env.REACT_APP_APP_KEY;
 
   const [recipes, setRecipes] = useState([]); 
+  const [search, setSearch] = useState('');
   
+  //this state is for only submitting when we click the search button
+  const [query, setQuery] = useState('chicken');
+
   //useeffect is just a function that is going to run every time the page loads
   useEffect( () => {
     getRecipes();
-  }, []); //the , [] will make it so it runs only once, when the page first loads
+  }, [query]); //the , [] will make it so it runs only once, when the page first loads
   //adding the state in the square brackets makes it so that it runs when the value changes
   
   const getRecipes = async () =>{
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${ID}&app_key=${KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${ID}&app_key=${KEY}`);
   
     const data = await response.json();
 
@@ -25,16 +29,26 @@ const App = () => {
     console.log(data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit = {getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
       {recipes.map(recipe => (
-        <Recipe 
+        <Recipe
           title ={recipe.recipe.label} 
           calories={recipe.recipe.calories} 
           image = {recipe.recipe.image}
